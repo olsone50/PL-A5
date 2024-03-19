97acf66addbabb74ff306e40ec76542305ad55aa
Evan Olson <119567125+olsone50@users.noreply.github.com>
2024-03-18 20:24:17 -0500

    Add files via upload

---
+++ a5.js
@@ -0,0 +1,39 @@
+/*
+            Your full names go here
+ */
+
+var fp = require('./fp');
+
+if ( ! exports ) {
+    var exports = [ ];
+}
+
+var getHighSales = function (list,min,threshold)
+{
+
+    var helper = function (  /* ??? */ )
+    {
+
+          /* implement this function */
+
+    };
+    
+    // hint: call the helper function here before returning
+};
+
+/* 
+
+// a few test cases
+
+console.log("> getHighSales([20,12,13,14,11,15], 10, 13);");
+getHighSales([20,12,13,14,11,15], 10, 13);
+console.log("> getHighSales([20,12,13,14,11,15], 10, 20);");
+getHighSales([20,12,13,14,11,15], 10, 20);
+console.log("> getHighSales([20,12,13,14,9,15], 10, 13);");
+getHighSales([20,12,13,14,9,15], 10, 13);
+console.log("> getHighSales([14], 10, 13);");
+getHighSales([14], 10, 13);
+
+*/
+
+exports.getHighSales = getHighSales;
